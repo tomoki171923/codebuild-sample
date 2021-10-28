5e10afe4603476e84be69db76ad0654548390dae
tomoki <tomoki171923@gmail.com>
2021-10-29 06:52:42 +0900

    add sample app for codebuild.

---
+++ main.py
@@ -0,0 +1,7 @@
+import time
+from pyutil.datetime_jp import now
+
+if __name__ == "__main__":
+    print(f"******************* SAMPLE START : {now()} *******************")
+    time.sleep(5)
+    print(f"******************* SAMPLE END : {now()} *******************")
